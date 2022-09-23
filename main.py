c251ec0776cf19a164258a8c7ce4d2da7f88bf4c
Agirre8 <inagi30@gmail.com>
2022-09-23 18:19:47 +0200

    ultimo update

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import Ejercicio1
 import Ejercicio2
 import Ejercicio3
 
-sum(Ejercicio1.matriz1)
-lista_numeros_1()
-lista_numeros_2()
-lista_numeros_3()
-lista_numeros_4()
-lista_numeros_5()
+Ejercicio1.sum(Ejercicio1.matriz1)
 
+
+Ejercicio3.lista_numeros_1()
+Ejercicio3.lista_numeros_2()
+Ejercicio3.lista_numeros_3()
+Ejercicio3.lista_numeros_4()
+Ejercicio3.lista_numeros_5()
+
+#Ejercicio2.cadena_texto()
