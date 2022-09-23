8fd607352309302382f7419815528a99d59cc498
Agirre8 <inagi30@gmail.com>
2022-09-23 17:25:12 +0200

    update

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,17 +1,27 @@
 
-def lista_numeros_1 (n):
-  list(range(n+1))
+def lista_numeros_1 ():
+  lista1=list(range(11))
+  print(lista1)
 
 def lista_numeros_2 ():
-  list(range(-10, 1))
+  lista2=list(range(-10, 1))
+  print(lista2)
 
 def lista_numeros_3 ():
-  list(range(0, 21, 2))
+  lista3=list(range(0, 21, 2))
+  print(lista3)
 
 def lista_numeros_4 ():
-  list(range(-19, 1, 2))
+  lista4=list(range(-19, 1, 2))
+  print(lista4)
 
 def lista_numeros_5 ():
-  list(range(0, 51, 5))
+  lista5=list(range(0, 51, 5))
+  print(lista5)
 
+lista_numeros_1()
+lista_numeros_2()
+lista_numeros_3()
+lista_numeros_4()
+lista_numeros_5()
 
--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import Ejercicio1
+import Ejercicio2
+import Ejercicio3
+
+sum(Ejercicio1.matriz1)
+lista_numeros_1()
+lista_numeros_2()
+lista_numeros_3()
+lista_numeros_4()
+lista_numeros_5()
 
